9374454738ee35263658062731da96762cf91f73
Travis Manning <travis.manning1@gmail.com>
2018-11-06 16:56:26 -0500

    added auth code; app.js grabbing dependencies;

---
+++ services/BudgetManagerAPI/app/api/auth.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose'),
+    jwt = require('jsonwebtoken'),
+    config = require('@config');
+
+const api = {};
+
+api.login = (User) => (req, res) => {
+    User.findOne({ username: req.body.username }, (error, matches) => {
+        if (error) throw error;
+
+        if (!user) res.status(401).send({ success: false, message: 'Authentication failed.'});
+        else {
+            user.comparePassword(req.body.password, (error, matches) => {
+                if (matches && !error) {
+                    const token = jwt.sign({ user }, config.secret);
+                    res.json({ success: true, message: 'Token granted', token});
+                } else {
+                    res.status(401).send({ success: false, message: 'Authentication failed.', token })
+                }
+            });
+        }
+    });
+}
+
+api.verify = (headers) => {
+    if (headers && headers.authorization) {
+        const split = headers.authorization.split(' ');
+        if (split.length === 2) return split[1];
+        else return null;
+    } else return null;
+}
+
+module.exports = api;
