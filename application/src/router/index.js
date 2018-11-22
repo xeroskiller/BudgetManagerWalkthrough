f0efb89e898cfc8c1e3a561d0dec45bf5b988daf
Travis Manning <travis.manning1@gmail.com>
2018-11-21 21:10:27 -0500

    template cleanup and first route def;

--- application/src/router/index.js
+++ application/src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Authentication from '@/components/pages/Authentication/Authentication'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      name: 'Authentication',
+      component: Authentication
     }
   ]
 })
